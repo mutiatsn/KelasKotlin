9623c96c5c3922088b889485543e7a77137abee4
mutiatsn <mtsaniaz@gmail.com>
2022-10-04 15:46:15 +0700

    Latihan

---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val dataInt = 5
+    val dataString = "Halo Polban"
+    val dataBoolean = true
+
+    var dataIntVar = 10
+
+    //const val KONSTAN_DATA = "Data"
+
+    //dataInt = 6
+    dataIntVar = 8
+
+    println(dataInt.plus(3))
+    println(dataIntVar)
+
+    println("Ini data dengan val $dataInt")
+    println("Ini data dengan val $dataString")
+}
---
+++ src/main/kotlin/Operation.kt
@@ -0,0 +1,10 @@
+fun main () {
+
+    val total = 1 + 2;
+    println(total)
+
+    val isTrue = true
+    val isFalse = false
+
+    println(isTrue && isFalse)
+}
