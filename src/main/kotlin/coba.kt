25b766a31cc986f3127c08f8424e232475681bc6
Mutia Tsani <114630859+mutiatsn@users.noreply.github.com>
2022-10-04 16:31:04 +0700

    Update coba.kt

--- src/main/kotlin/coba.kt
+++ src/main/kotlin/coba.kt
@@ -1,13 +1,1 @@
-fun main (args : Array<String>) {
 
-    var hour: Int
-
-    hour = 8
-
-    var total = when {
-        hour >= 24 -> (15 * (hour/24)) + (0.5 * (hour%24))
-        hour > 5 && hour <24 -> 5 + ((hour-5)*0.5)
-        else -> hour.toDouble()
-    }
-    println(total)
-}
